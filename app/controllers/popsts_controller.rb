3505773aae6305304d82f60c0759fe179c97914a
tenndou8110 <139302819+tenndou8110@users.noreply.github.com>
2023-07-12 14:37:29 +0900

    first commit

---
+++ app/controllers/popsts_controller.rb
@@ -0,0 +1,2 @@
+class PopstsController < ApplicationController
+end
---
+++ test/controllers/popsts_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PopstsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
